442abc946196f7cf23bdc7b966a420a66c16c94e
reireias <reireiAs@gmail.com>
2017-12-01 12:55:27 +0900

    add delay setting

--- web/bs-config.js
+++ web/bs-config.js
@@ -11,7 +11,9 @@ module.exports = {
         }
     },
     files: [
-        './dist/*',
-        './src/**'
-    ]
+        './dist/*'
+    ],
+    watchOptions: {
+        awaitWriteFinish: true
+    }
 };
