aea57e23fd0641cc9241d2528c8d5c822762c9b3
reireias <reireiAs@gmail.com>
2017-12-06 23:31:04 +0900

    add Dockerfile

--- main.go
+++ main.go
@@ -15,5 +15,7 @@ func HelloHandler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/hello", HelloHandler)
+	router.PathPrefix("/web/").Handler(http.StripPrefix("/web/", http.FileServer(http.Dir("./web"))))
+	router.PathPrefix("/public/").Handler(http.StripPrefix("/public/", http.FileServer(http.Dir("./web/public"))))
 	http.ListenAndServe(":8080", router)
 }
